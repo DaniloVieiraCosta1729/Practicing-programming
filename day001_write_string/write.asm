36aad71a80501aa58dd55ba18fcf29edffca10c2
Danilo <danilo.11235@outlook.com>
2026-02-04 06:41:40 -0300

    Add an read and newline macros.

--- day001_write_string/write.asm
+++ day001_write_string/write.asm
@@ -1,13 +1,25 @@
 %include "../D_lib/Macros/IO.inc"
 
 section .data
-    text db "Testing", 10
-    size equ $-text
+    text0 db "Digit something, please: ", 10
+    size0 equ $-text0
+
+    text1 db "You said: "
+    size1 equ $-text1
+
+section .bss
+    input0 resb 50
+    input0size resb 1
 
 section .text
     global _start
 _start:
-    write text, size
+    WRITE text0, size0
+    READ input0, 50
+    mov [input0size], al
+    WRITE text1, size1
+    WRITE input0, input0size
+    NEWLINE
 
     mov rax, 60
     xor rdi, rdi
