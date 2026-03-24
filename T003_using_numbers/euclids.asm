85e2f42862d1c7e9798079df72226d59f743f988
Danilo <danilo.11235@outlook.com>
2026-03-23 23:12:05 -0300

    Solution to gcd with euclids algorithm

--- T003_using_numbers/euclids.asm
+++ T003_using_numbers/euclids.asm
@@ -1,10 +1,12 @@
 %include "../D_lib/Macros/IO.inc"
+%include "../D_lib/Procedures/Parser.inc"
+%include "../D_lib/Macros/Math.inc"
 
 section .data
     welcome db 9,9,"Euclids Algorithm",10,10
     size_welcome equ $-welcome
 
-    info01 db "Digit two numbers to see their gcd", 10
+    info01 db "Digit two numbers to see their gcd...", 10
     size_info01 equ $-info01
 
     info02 db "first number >>> "
@@ -13,17 +15,21 @@ section .data
     info03 db "second number >>> "
     size_info03 equ $-info03
 
+    info04 db "The GCD between these number is "
+    size_info04 equ $-info04
+
 section .bss
-    input01 resw 1
+    input01 resd 1
     input01_text resb 16
     size_input01 resb 1
 
-    input02 resw 1
+    input02 resd 1
     input02_text resb 16
     size_input02 resb 1
 
-    gcd resw 1
-    size_gcd resb 1
+    gcd_actual_result resd 1
+    gcd_actual_size resd 1
+    gcd_text_result resb 16
 
 section .text
     global _start
@@ -31,8 +37,47 @@ section .text
 _start:
     WRITE welcome, size_welcome
     WRITE info01, size_info01
-    WRITE info02, size_info02
 
+    NEWLINE
+
+    WRITE info02, size_info02
+    READ input01_text, 16
+    mov [size_input01], rax
+    mov rsi, rax 
+    mov rdi, input01_text
+    ; string2integer(rdi = buffer, rsi = size of the string)
+    ; returns in rax
+    call string2integer 
+    mov dword [input01], eax
+
+    NEWLINE
+
+    WRITE info03, size_info03
+    READ input02_text, 16
+    mov [size_input02], rax
+    mov rsi, rax 
+    mov rdi, input02_text
+    ; string2integer(rdi = buffer, rsi = size of the string)
+    ; returns in rax
+    call string2integer
+    mov dword [input02], eax    
+
+    mov rax, qword [input01]
+    mov rbx, qword [input02]
+    gcd rax, rbx
+    mov dword [gcd_actual_result], eax
+
+    mov esi, eax
+    mov rdi, gcd_text_result
+    mov rdx, 16
+    call integer2string
+    mov r8, rax 
+    mov [gcd_actual_size], r9
+
+    WRITE info04, size_info04
+    WRITE gcd_text_result, r8
+    NEWLINE
+    
 .end:
     mov rax, 60
     xor rdi, rdi
